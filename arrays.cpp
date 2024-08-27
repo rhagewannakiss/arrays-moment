d3117fad8046acb7a01a49d536dafd086525c02a
rhage <139259164+rhagewannakiss@users.noreply.github.com>
2024-08-27 20:07:11 +0300

    добавлена сумма

--- arrays.cpp
+++ arrays.cpp
@@ -8,6 +8,7 @@
 
 void output_mass(int* data, size_t length, size_t width);
 void output_sq(int* data, size_t length, size_t width);
+void sum_arrays(int* data_1, int* data_2, size_t length, size_t width);
 
 //------------------MAIN-------------------------------------------------------------------
 
@@ -16,15 +17,23 @@ int main(void)
     size_t length = 5;
     size_t  width = 5;
 
-    int data[5][5] = {{0, 5, 2, 3, 2},
-                      {5, 0, 3, 1, 1},
-                      {2, 3, 0, 4, 0},
-                      {3, 1, 4, 0, 3},
-                      {2, 1, 0, 3, 0}};
+    int data_1[5][5] = {{0, 5, 2, 3, 2},
+                        {5, 0, 3, 1, 1},
+                        {2, 3, 0, 4, 0},
+                        {3, 1, 4, 0, 3},
+                        {2, 1, 0, 3, 0}};
 
-    output_mass(*data, length, width);
+    int data_2[5][5] = {{-4, 3, 7, 2, 2},
+                        {5, -2, 6, 0, 2},
+                        {6, 9, 5, 4,  0},
+                        {-3, 7, 5, 8, 5},
+                        {5, 6, 7, -7, 7}};
 
-    output_sq(*data, length, width);
+   /* output_mass(*data_1, length, width);
+
+    output_sq(*data_1, length, width); */
+
+    sum_arrays((int*)data_1, (int*)data_2, length, width);
 
     system("color 0B");
 
@@ -64,3 +73,19 @@ void output_sq(int *data, size_t length, size_t width)
         printf("\n");
     }
 }
+
+void sum_arrays(int* data_1, int* data_2, size_t length, size_t width)
+{
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0;  j < width; j++)
+        {
+            printf("%d ", *((int*)data_1 + i*width + j) + *((int*)data_2 + i*width + j) );
+        }
+        printf("\n");
+    }
+
+}
+
+
